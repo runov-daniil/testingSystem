fd259c46d14d633ae6af79cefc331b974a5fa18b
runov-daniil <runov-daniil@yandex.ru>
2017-02-14 14:15:48 +0300

    Разработка взаимодействия с БД

--- server/src/server/dataBase.java
+++ server/src/server/dataBase.java
@@ -1,5 +1,17 @@
 package server;
 
+import com.sun.corba.se.pept.transport.Connection;
+import java.sql.*;
+
 public class dataBase {
-    
+    private static Connection conn;   
+    public static void getConnection() throws ClassNotFoundException, SQLException{
+        conn = null;
+        Class.forName("org.sqlite.JDBC");
+        conn = (Connection) DriverManager.getConnection("jdbc:sqlite:data.db");
+    }
+    //Закрытие базы
+    public  static void closeConnection() throws ClassNotFoundException, SQLException {
+        conn.close();
+    }    
 }
